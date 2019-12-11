4145666788fb8a4462a0f3ec2472975ac35a72f9
huangyujie-art <huangyujie1993@126.com>
2019-12-11 16:17:05 +0800

    Initial commit

---
+++ Assets/TestDll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DLLtest;
+public class TestDll : MonoBehaviour
+{
+   
+    void Start()
+    {
+        DLLClass dc = new DLLClass();
+        Debug.Log(dc.Check());
+        Debug.Log(dc.Add(1,2));
+    }
+
+  
+}
